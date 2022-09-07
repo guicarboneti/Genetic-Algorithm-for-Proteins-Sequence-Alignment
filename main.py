d78cd0d116541c9bb36a00e0e3dd7c6c86dbd776
Leon A. Okida Gonçalves <laog19@inf.ufpr.br>
2022-09-07 18:55:58 -0300

    [add] - add dict with pam250 matrix values

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from deap import creator, base, tools, algorithms
+from numpy import matrix
 from getSequences import getSequences
 import random
+from pam250 import pam250
+
+PAM250 = pam250.matrix
 
 #getting list of sequences
 sequences = getSequences()
