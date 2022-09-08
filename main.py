31018afedae2808f1f29c421411157fca7ad99c2
Leon A. Okida Gonçalves <laog19@inf.ufpr.br>
2022-09-08 16:48:07 -0300

    [add] - continued development of genetic algorithm, need to implement objective function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from deap import creator, base, tools, algorithms
 from numpy import matrix
 from getSequences import getSequences
 import random
+from objectiveFunction import evaluate
 from population import generateSeqList, normalizeSize
 
 sequences = []
@@ -33,3 +34,41 @@ toolbox.register("sequences", generate_individual)
 toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.sequences)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 pop = toolbox.population(n=POPSIZE)
+
+#adding objective function
+toolbox.register("evaluate", evaluate)
+
+CXPB, MUTPB, NGEN = 0.5, 0.2, 40
+
+fitnesses = map(toolbox.evaluate, pop)
+for ind, fit in zip(pop, fitnesses):
+    ind.fitness.values = fit
+
+for g in range(NGEN):
+    # Select the next generation individuals
+    offspring = toolbox.select(pop, len(pop))
+    # Clone the selected individuals
+    offspring = map(toolbox.clone, offspring)
+
+    # Apply crossover and mutation on the offspring
+    for child1, child2 in zip(offspring[::2], offspring[1::2]):
+        if random.random() < CXPB:
+            toolbox.mate(child1, child2)
+            del child1.fitness.values
+            del child2.fitness.values
+
+    for mutant in offspring:
+        if random.random() < MUTPB:
+            toolbox.mutate(mutant)
+            del mutant.fitness.values
+
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+    fitnesses = map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+
+    # The population is entirely replaced by the offspring
+    pop[:] = offspring
+
+print(pop)
--- objectiveFunction.py
+++ objectiveFunction.py
@@ -1,6 +1,6 @@
 from pam250 import pam250
 PAM250 = pam250.matrix
 
-def evaluate(alignment, numSequences):
+def evaluate(alignment):
     #deve retornar tupla
     return (0, 0)
