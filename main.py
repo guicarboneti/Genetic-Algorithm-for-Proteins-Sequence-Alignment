071f864b7381ed4f4396571a9fccb31ee1f5070a
Leon A. Okida Gonçalves <laog19@inf.ufpr.br>
2022-09-13 21:27:37 -0300

    [add] - prints generation data to csv file

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from fileinput import close
-from deap import creator, base, tools, algorithms
-from numpy import matrix, sort
+import statistics
 from getSequences import getSequences
 import random
 from objectiveFunction import evaluate
@@ -15,24 +13,29 @@ def scoreKey(e):
     return e["score"]
 
 def printPopulation(pop):
+    alignmentFile = open("alignment.txt", "w")
     algNumber = 1
     for i in pop:
-        print("Alinhamento", algNumber)
+        print("Alinhamento", algNumber, file=alignmentFile)
         alignment = []
         for j in i['alignment']:
             sequence = ''
             sequence = sequence.join(j)
             alignment.append(sequence)
 
-        print(alignment)
-        print("Score:", str(i['score']))
+        print(alignment, file=alignmentFile)
+        print("Score:", str(i['score']), file=alignmentFile)
         algNumber = algNumber + 1
+    alignmentFile.close()
 
 #getting list of sequences
 sequences = getSequences()
 
 #shuffling list
 random.shuffle(sequences)
+
+resultfile = open("result.csv", "w")
+print("generation,highest_fitness,average_fitness,standard_deviation", file=resultfile)
 
 #getting initial population
 sequences = generateSeqList(sequences, NUMSEQUENCES)
@@ -48,10 +51,13 @@ for i in range(POPSIZE):
     pop.append(individual)
 
 for i in range(NUMGENERATIONS):
-    print("Geração", i + 1)
+    print("Geração", i)
     for individual in pop:
         if individual["score"] == 0:
             individual["score"] = evaluate(individual["alignment"])
+
+    popScores = [i['score'] for i in pop]
+    print(i, ",", max(popScores), ",", sum(popScores) / len(popScores), ",", statistics.pstdev(popScores), file=resultfile) 
 
     # remove the worst half
     pop.sort(key=scoreKey)
@@ -77,5 +83,9 @@ for i in range(NUMGENERATIONS):
 for individual in pop:
         if individual["score"] == 0:
             individual["score"] = evaluate(individual["alignment"])
+
+print(NUMGENERATIONS, ",", max(popScores), ",", sum(popScores) / len(popScores), ",", statistics.pstdev(popScores), file=resultfile) 
+
+resultfile.close()
 
 printPopulation(pop)
